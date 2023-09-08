dbbed46e0cb651454ac3a5a9ce3d06ade05107a5
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-09-08 23:46:37 +0900

    Refactor: Move responsibility of tableView data source to Controller

--- quick-reminder/Controller/ReminderListViewController.swift
+++ quick-reminder/Controller/ReminderListViewController.swift
@@ -14,7 +14,32 @@ class ReminderListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "登録中のリマインダー"
-        view = ReminderListView()
+        
+        view = {
+            let reminderListView = ReminderListView()
+            reminderListView.dataSource = self
+            return reminderListView
+        }()
     }
 
+}
+
+extension ReminderListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = "aaa"
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = "aaa"
+        }
+        return cell
+    }
+    
 }
--- quick-reminder/View/ReminderListView.swift
+++ quick-reminder/View/ReminderListView.swift
@@ -15,6 +15,12 @@ class ReminderListView: UIView {
         return reminderTableView
     }()
     
+    weak var dataSource: UITableViewDataSource? {
+        didSet {
+            reminderTableView.dataSource = dataSource
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemMint
@@ -29,32 +35,14 @@ class ReminderListView: UIView {
             reminderTableView.topAnchor.constraint(equalTo: topAnchor),
             reminderTableView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
-        
-        reminderTableView.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-}
-
-extension ReminderListView: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        if #available(iOS 14.0, *) {
-            var content = cell.defaultContentConfiguration()
-            content.text = "aaa"
-            cell.contentConfiguration = content
-        } else {
-            cell.textLabel?.text = "aaa"
-        }
-        return cell
+    func reloadData() {
+        reminderTableView.reloadData()
     }
     
 }
