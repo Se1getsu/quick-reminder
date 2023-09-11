798402a13724376a6b12c06900327ab93334bc6f
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-09-11 18:01:24 +0900

    Fix logic in NotificationDateCalculator

--- quick-reminder/Util/NotificationDateCalculator.swift
+++ quick-reminder/Util/NotificationDateCalculator.swift
@@ -15,9 +15,18 @@ final class NotificationDateCalculator {
     func calculate(from date: Date) -> Date {
         let calendar = Calendar.current
         var dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        
+        func calculateMinuteTime(from date: Date) -> Int {
+            let components = Calendar.current.dateComponents([.hour, .minute], from: date)
+            return components.hour! * 60 + components.minute!
+        }
+        
         let now = Date()
         let nowDay = calendar.dateComponents([.day], from: now).day!
-        dateComponents.day = date <= now ? nowDay + 1 : nowDay
+        dateComponents.day = nowDay
+        if calculateMinuteTime(from: date) <= calculateMinuteTime(from: now) {
+            dateComponents.day! += 1
+        }
         return calendar.date(from: dateComponents)!
     }
 }
