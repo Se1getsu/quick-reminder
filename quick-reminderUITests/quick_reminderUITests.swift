30dc548675f0ad45ef9090db32ee61bc69ffe6f5
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-09-19 17:19:38 +0900

    Test: Create UITest test_リマインダー編集

--- quick-reminderUITests/quick_reminderUITests.swift
+++ quick-reminderUITests/quick_reminderUITests.swift
@@ -57,6 +57,41 @@ final class quick_reminderUITests: XCTestCase {
         XCTAssertTrue(reminderListTableView.isHittable)
         XCTAssertEqual(reminderListTableView.cells.count, 1)
     }
+    
+    func test_リマインダー編集() throws {
+        let app = XCUIApplication()
+        app.launchArguments.append("-useUITestMockRepository")
+        app.launch()
+        
+        // 新規作成
+        let addButton = app.buttons["Add Reminder Bar Button"].firstMatch
+        addButton.tap()
+        
+        // 編集
+        let titleTextField = app.textFields["Reminder Title Text Field"].firstMatch
+        let timeDatePicker = app.datePickers["Reminder Time Date Picker"].firstMatch
+        titleTextField.tap()
+        titleTextField.typeText("サンプル文字列")
+        timeDatePicker.tap()
+        app.pickerWheels.element(boundBy: 0).adjust(toPickerWheelValue: "12")
+        app.pickerWheels.element(boundBy: 1).adjust(toPickerWheelValue: "34")
+        // timeDatePickerを閉じる
+        let coordinate = app.coordinate(withNormalizedOffset: CGVector(dx: 0.1, dy: 0.1))
+        coordinate.tap()
+        
+        // 戻るボタンを押す
+        let saveButton = app.navigationBars.buttons["Reminder Edit Save Button"].firstMatch
+        XCTAssertTrue(saveButton.exists)
+        XCTAssertTrue(saveButton.isHittable)
+        saveButton.tap()
+
+        // 編集した内容を確認
+        let reminderListTableView = app.tables["Reminder List Table View"].firstMatch
+        XCTAssertEqual(reminderListTableView.cells.count, 1)
+        let cell = reminderListTableView.cells.element(boundBy: 0)
+        XCTAssertEqual(cell.staticTexts.element(boundBy: 0).label, "サンプル文字列")
+        XCTAssertTrue(cell.staticTexts.element(boundBy: 1).firstMatch.label.contains("12:34"))
+    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
