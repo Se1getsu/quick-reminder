e5dd08145120c205d80cf20b127b466411cbc068
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-10-12 21:57:54 +0900

    Test: Move unit test files to correct directory

---
+++ quick-reminderTests/ReminderEditPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderEditPresenter.swift
+//  quick-reminderTests
+//  
+//  Created by Seigetsu on 2023/10/12
+//  
+//
+
+import XCTest
+
+final class ReminderEditPresenter: XCTestCase {
+
+}
---
+++ quick-reminderTests/ReminderListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderListPresenter.swift
+//  quick-reminderTests
+//  
+//  Created by Seigetsu on 2023/10/12
+//  
+//
+
+import XCTest
+
+final class ReminderListPresenter: XCTestCase {
+
+}
